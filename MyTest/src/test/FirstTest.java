3abf0f4abe9206e7e91129fc2d270b4878254f9f
DUCVA <hex0r@me.com>
2014-11-23 01:41:27 +0700

    Refactor Code

---
+++ MyTest/src/test/FirstTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test1() {
+		String temp = null;
+		Assert.assertNull("Null", temp);
+	}
+
+}
